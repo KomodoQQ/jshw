b9275702d0d0ff7ee758d75b7d33406de92381bf
Mykhailo Kovalenko <Mykhailo_Kovalenko@epam.com>
2021-08-30 16:43:42 +0300

    fix

--- numbers/6.js
+++ numbers/6.js
@@ -9,47 +9,10 @@ function step(g, m, n) {
   for (let i = m; i <= n; i++) {
     if(isPrime(i)) {
       primes.push(i);
-      console.log(`Num ${i} is prime`)
+      if(primes.includes(i-g)) {
+        return [i-g, i];
+      }
     }
   }
-  let result = null;
-  for (let i = 1; i < primes.length; i++) {
-    if(primes[i] - primes[i-1] == g) {
-      return [primes[i-1], primes[i]]
-    }
-  }
-  console.log(result.slice(0,2))
-  return result;
-}
-
-/////////////////////////////////////////////
-/////////////////////////////////////////////
-
-const isPrime = (num) => {
-  for(let i = 2; i < num; i++)
-    if(num % i === 0) return false;
-  return num > 1;
-}
-
-function step(g, m, n) {
-  let primes = [];
-  let result = [];
-  for (let i = m; i <= n; i++) {
-    if(isPrime(i)) {
-      primes.push(i);
-    }
-  }
-  
-  primes.forEach((prime, index) => {
-    if(prime + g == primes[index+1]) {
-      result.push(prime, primes[index+1]);
-    }
-  })
-  
-  if(result) {
-    return result.slice(0, 2);
-  } else {
-    return null;
-  }
-
+  return null;
 }
